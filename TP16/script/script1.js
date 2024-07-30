cd27e940fc5265652e257c7f3967dd35eb29d02a
Pablo Deganis <pdeganis@gmail.com>
2024-07-30 17:17:57 -0300

    Inicio de Javascript

---
+++ TP16/script/script1.js
@@ -0,0 +1,12 @@
+console.log("Soy un log");
+console.info("Soy un info");
+console.warn("Soy un Warn");
+console.error("Soy un error");
+
+console.table({
+    Nombre: "Pablo",
+    Apellido: "Deganis",
+    DNI: 30278003,
+});
+
+window.prompt("Ingrese un dato:");
---
+++ TP16/script/script2.js
@@ -0,0 +1,38 @@
+let nombre;
+nombre = "Pablo";
+nombre = "Ignacio";
+console.log(nombre);
+
+const apellido = "Deganis";
+//apellido = "Villalba";
+console.log(apellido);
+
+let nombreCompleto = nombre + " " + apellido;
+console.log(nombreCompleto);
+
+//let dni = window.prompt("Ingrese su numero de DNI");
+//console.log("Su DNI es: " + dni);
+
+let numero1 = 6;
+let numero2 = 4;
+let suma = numero1 + numero2;
+console.log(suma);
+
+/* let numero3 = window.prompt("Ingrese el primer número");
+numero3 = Number(numero3);
+let numero4 = window.prompt("Ingrese el segundo número");
+numero4 = Number(numero4);
+let sumaDeInputs = numero3 + numero4;
+console.log(sumaDeInputs); */
+
+// Scoope
+let num = 12;
+console.log(num)
+{
+    let num = 13;
+    console.log(num);
+    let letra = "A";
+    console.log(letra);
+}
+console.log(num);
+console.log(letra);
